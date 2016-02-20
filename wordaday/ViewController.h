55777891f59892e4e60cccd894a4c3c56af60032
chaikon <achai@andrew.cmu.edu>
2016-02-19 23:50:25 -0500

    Initial Commit

---
+++ wordaday/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wordaday
+//
+//  Created by Alexander Chai on 2/19/16.
+//  Copyright © 2016 chaikon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
